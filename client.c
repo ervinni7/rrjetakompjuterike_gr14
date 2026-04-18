f001cf0cc652ce9c1f2e2d137a953beb94a1977f
ernesamavraj <ernesa.mavraj@student.uni-pr.edu>
2026-04-18 17:27:40 +0200

    Modifikime te vogla ne client.c

--- client.c
+++ client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <string.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -12,7 +13,7 @@ void handle_download(SOCKET sock, char *header){
     char filename[128];
     long size;
 
-    sscanf(header, "DOWNLOAD_START:%[^:]:%ld",filename,&size);
+    sscanf(header, "DOWNLOAD_START:%[^:]:%ld", filename, &size);
 
     FILE *f = fopen(filename, "wb");
     if(!f){
@@ -22,15 +23,15 @@ void handle_download(SOCKET sock, char *header){
     char buf[BUF_SIZE];
     long received =0;
 
-    while(received<size){
-        int n = recv(sock, buf, sizeof(buf),0);
-        if(n<=0) break;
+    while(received < size){
+        int n = recv(sock, buf, sizeof(buf), 0);
+        if(n <= 0) break;
 
-        fwrite(buf,1,n,f);
-        received +=n;
+        fwrite(buf , 1, n, f);
+        received += n;
     }
     fclose(f);
-    printf("File u shkarkua: %s (%ld bytes)\n",filename,received);
+    printf("File u shkarkua: %s (%ld bytes)\n", filename, received);
 }
  
 void handle_upload(SOCKET sock, char *filename){
@@ -39,12 +40,12 @@ void handle_upload(SOCKET sock, char *filename){
         printf("File nuk ekziston\n");
         return;
     }
-    fseek(f,0,SEEK_END);
+    fseek(f, 0, SEEK_END);
     long size = ftell(f);
     rewind(f);
 
     char header[64];
-    sprintf(header, "SIZE%ld\n",size);
+    sprintf(header, "SIZE:%ld\n", size);
     send(sock,header,strlen(header),0);
 
     char buf[BUF_SIZE];
@@ -63,48 +64,73 @@ SOCKET sock;
 struct sockaddr_in server;
 char buffer[BUF_SIZE];
 
-WSAStartup(MAKEWORD(2,2), &wsa);
+if(WSAStartup(MAKEWORD(2,2), &wsa)!=0){
+    printf("Gabim me WSAStartup\n");
+    return 1;
+}
 
 sock = socket(AF_INET, SOCK_STREAM, 0);
+if(sock==INVALID_SOCKET){
+    printf("Gabim ne socket!\n");
+    WSACleanup();
+    return 1;
+}
 
 server.sin_family = AF_INET;
 server.sin_port = htons(PORT);
 inet_pton(AF_INET, SERVER_IP, &server.sin_addr);
 
-connect(sock, (struct sockaddr*)&server, sizeof(server));
+if(connect(sock, (struct sockaddr*)&server, sizeof(server))<0){
+    printf("Nuk u lidh me serverin\n");
+    closesocket(sock);
+    WSACleanup();
+    return 1;
+};
 
 printf("U lidh me serverin\n");
 
 int n = recv(sock, buffer, sizeof(buffer)-1, 0);
+if(n>0){
 buffer[n] = '\0';
 printf("%s\n", buffer);
+}
 
 while (1) {
-printf("> ");
-fgets(buffer, sizeof(buffer), stdin);
+    printf("> ");
+    fgets(buffer, sizeof(buffer), stdin);
 
-send(sock, buffer, strlen(buffer), 0);
+    if (strncmp(buffer, "/exit", 5) == 0) {
+        break;
+    }
 
-n = recv(sock, buffer, sizeof(buffer)-1, 0);
-if (n <= 0) break;
+    send(sock, buffer, strlen(buffer), 0);
 
-buffer[n] = '\0';
+    while ((n = recv(sock, buffer, sizeof(buffer)-1, 0)) > 0) {
+        buffer[n] = '\0';
 
-// DOWNLOAD
-if (strncmp(buffer, "DOWNLOAD_START", 14) == 0) {
-handle_download(sock, buffer);
-continue;
-}
+        // DOWNLOAD
+        if (strncmp(buffer, "DOWNLOAD_START", 14) == 0) {
+            handle_download(sock, buffer);
+            break;  
+        }
 
-// UPLOAD
-if (strncmp(buffer, "GATI_PER_UPLOAD", 15) == 0) {
-char filename[128];
-sscanf(buffer, "GATI_PER_UPLOAD:%s", filename);
-handle_upload(sock, filename);
-continue;
-}
+        // UPLOAD
+        if (strncmp(buffer, "GATI_PER_UPLOAD", 15) == 0) {
+            char filename[128];
+            sscanf(buffer, "GATI_PER_UPLOAD:%s", filename);
+            handle_upload(sock, filename);
+            break;  
+        }
 
-printf("%s\n", buffer);
+        printf("%s", buffer);
+
+        if (n < BUF_SIZE - 1) break;
+    }
+
+    if (n <= 0) {
+        printf("\nServeri u shkeput.\n");
+        break;
+    }
 }
 
 closesocket(sock);
